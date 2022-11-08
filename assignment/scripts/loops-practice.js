864eaa62aed1b34eeb02d19edf59ccfe393d5738
Tou Fue Yang <touyangbiz@gmail.com>
2022-11-08 02:21:31 -0600

    so far so easy

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -1,6 +1,6 @@
 console.log('****** Loops Practice *******');
 
-
+console.log('---1 example start---');
 // 1. 'for' loop
 console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
@@ -9,22 +9,60 @@ console.log('count from 0 to 3');
 for (let i=0; i<4; i++) {  
   console.log(i);
 }
+console.log('---1 example finished---');
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+console.log('----My work of 1a start----');
+
+for( let i=0; i<5; i++){
+  console.log(i);
+}
+
+
+console.log('---My work of 1a finished---');
+
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+console.log('----My work of 1b start----');
+
+for( let i=3; i<5; i++){
+  console.log(i);
+}
+
+console.log('----My work of 1b finished----');
+
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+console.log('----My work of 1c start----');
+for(let n=2; n<=10; n+= 2){
+  //console.log(n);
+  //if(n%2 == 0){
+    console.log("%d/n",n);
+  }//https://codeforwin.org/2015/06/c-program-to-print-all-even-numbers-between-1-to-100.html how i found the formula but it still print the odd numbers. 
+//Im not sure it is showing n/n??
+
+  for(let n=2; n<=10; n=n+2){
+    console.log('print all even numbers to ten', n);
+  }
+console.log('----My work of 1c finished----');
+
+
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+console.log('----My work of 1d start----');
 
+for(let n=5; n>=0; n--){
+  console.log(n);
+}
+
+console.log('----My work of 1d finished----');
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -32,6 +70,13 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+console.log('----My work of 2a start----');
+
+for( fameous of stars){
+  console.log('Some stars:', stars);
+}
+
+console.log('----My work of 2a finished----');
 
 
 // 3. While loops  
@@ -39,6 +84,11 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+console.log('----My work of 3a start----');
+
+console.log('----My work of 3a finished----');
+
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
